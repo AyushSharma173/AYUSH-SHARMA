7e3a18dcda9ddb9bd8cf93f8c835fd5422675c5d
Ayush Sharma <ayushsharma@ayushs-macbook-pro.local>
2018-07-30 19:50:11 +0530

    Initial Commit

---
+++ AYUSH SHARMA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AYUSH SHARMA
+//
+//  Created by Ayush Sharma on 7/30/18.
+//  Copyright © 2018 Ayush Sharma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
